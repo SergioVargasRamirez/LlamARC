c01ed8c2ac55f31e221b05281af4206c88acbb5f
Sergio Vargas R. <sergio.vargas@biodatum.io>
2025-07-24 14:58:52 +0200

    edit run_server

--- llama.cpp.scripts/run_server_llama.sh
+++ llama.cpp.scripts/run_server_llama.sh
@@ -6,7 +6,7 @@ export SYCL_CACHE_PERSISTENT=1
 export ZES_ENABLE_SYSMAN=1
 
 #llama
-/llama.cpp/server -m /models/Meta-Llama-3-8B-Instruct/Meta-Llama-3-8B_q4_K_M.gguf -c 4096 -ngl 99 -sm none -mg 0 --host 0.0.0.0 --port 8081
+/llama.cpp/server -m /models/Meta-Llama-3-8B-Instruct/Meta-Llama-3-8B-Instruct-Q5_K_M.gguf -c 2048 -ngl 99 -sm none -mg 0 --host 0.0.0.0 --port 8081
 
 #code llama
-#/llama.cpp/server -m /models/CodeLlama-13b-Instruct-hf/CodeLlama-13b-Instruct-hf-13B-Q4_K_M.gguf -c 4096 -ngl 99 -sm none -mg 0 --host 0.0.0.0 --port 8081
+#/llama.cpp/server -m /models/CodeLlama-13b-Instruct-hf/CodeLlama-13b-Instruct-hf-13B-Q4_K_M.gguf -c 2048 -ngl 99 -sm none -mg 0 --host 0.0.0.0 --port 8081
